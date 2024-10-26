beb8bfdcee5177d307ebf940b17e76463af7054d
Martin Sidorov <martin.sidorov27@gmail.com>
2024-10-26 16:09:44 +0300

    feat: remove unnecessary sections and fix colors

--- resources/js/main.js
+++ resources/js/main.js
@@ -43,66 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  fetch("members.json")
-    .then((response) => response.json())
-    .then((data) => populateMembers(data))
-    .catch((error) => console.error("Error:", error));
-
-  function populateMembers(members) {
-    // Sort members so that the CEO is in the center
-    const ceoIndex = members.findIndex((member) =>
-      member.profession.includes("CEO")
-    );
-    const ceoMember = members[ceoIndex];
-    members.splice(ceoIndex, 1);
-    const centerIndex = Math.floor(members.length / 2);
-    members.splice(centerIndex, 0, ceoMember);
-
-    const membersDiv = document.getElementById("team-members");
-    members.forEach((member, index) => {
-      const memberDiv = document.createElement("div");
-      membersDiv.style.display = "flex";
-      membersDiv.style.flexWrap = "wrap";
-      membersDiv.style.justifyContent = "space-around";
-      memberDiv.className =
-        "w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-4 flex flex-col items-center text-center";
-      memberDiv.style.boxShadow = "0 4px 8px 0 rgba(0,0,0,0.2)";
-      memberDiv.style.transition = "0.3s";
-      memberDiv.style.borderRadius = "5px"; // Rounded corners
-      memberDiv.style.margin = "0 20px"; // Increase horizontal space between boxes
-
-      const img = document.createElement("img");
-      img.className = "w-32 h-32 rounded-full mb-4 object-cover"; // Same size for all images
-      img.src = member.picture;
-      img.alt = `${member.name}'s picture`;
-      img.style.width = "150px"; // Set width
-      img.style.height = "150px"; // Set height
-
-      const name = document.createElement("h2");
-      name.className = "text-xl font-bold mb-2";
-      name.textContent = member.name;
-
-      const profession = document.createElement("p");
-      profession.className = "mb-2";
-      profession.textContent = member.profession;
-
-      const portfolio = document.createElement("a");
-      portfolio.className = "block";
-      portfolio.href = member.portfolio;
-      portfolio.innerHTML = '<i class="fas fa-briefcase"></i> Portfolio';
-      portfolio.style.color = "#ffffff"; // White color for links
-
-      memberDiv.style.margin = "0 35px 0px 30px"; // Increase space between members
-      memberDiv.appendChild(img);
-      memberDiv.appendChild(name);
-      memberDiv.appendChild(profession);
-
-      memberDiv.appendChild(portfolio);
-
-      membersDiv.appendChild(memberDiv);
-    });
-  }
-
   // Loader
   window.addEventListener("load", function () {
     setTimeout(function () {
